827665673d51c71bb0d9368d25ab708ceb21aa8d
willkennedy2000 <william.kennedy@carbongraph.io>
2023-01-23 14:50:10 -0500

    added loading icons and intuitive response messages

--- docqshare/src/components/topnavigation/index.tsx
+++ docqshare/src/components/topnavigation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BsArrowLeft } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { AiOutlineHome } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const TopNavigation = () => {
     var Banner = require('../../assets/DocuShareBanner.jpg');
@@ -8,12 +8,13 @@ const TopNavigation = () => {
         <div className="w-full h-full">
             <div className="w-screen fixed bg-white shadow-2xl rounded-lg py-2">
                 <div className="w-full h-full grid grid-cols-3">
-                    <div className="h-full w-fit flex flex-col justify-center pl-2">
+                    <div className="h-full w-fit flex flex-col justify-center pl-4">
                         <button className=" bg-queens-blue p-2 rounded-md hover:bg-blue-400">
                             <Link to='/' className=''>
                                 <div className="flex">
-                                    <div className="pt-2 pr-1"><BsArrowLeft color='white'/></div>
-                                    <h1 className="text-white text-bold text-xl pr-1">Home page</h1>
+                                    {/* <div className="pt-2 pr-1"><BsArrowLeft color='white'/></div>
+                                    <h1 className="text-white text-bold text-xl px-1">Home page</h1> */}
+                                    <AiOutlineHome color='white' size={25}/>
                                 </div>
                             </Link>
                         </button>
