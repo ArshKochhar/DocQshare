cd736b32d30b6c68fb5adbceb61070d7ab0c1b35
willkennedy2000 <william.kennedy@carbongraph.io>
2023-02-16 18:18:58 -0500

    adding file popup structure

--- docqshare/src/components/account/addFIle.tsx
+++ docqshare/src/components/account/addFIle.tsx
@@ -25,7 +25,8 @@ export default function AddFile() {
     }
 
     function closeModal() {
-        setIsOpen(false)
+        setIsOpen(false);
+        setCurrentFile(null);
     }
 
     function openModal() {
@@ -96,44 +97,51 @@ export default function AddFile() {
                     leaveFrom="opacity-100 scale-100"
                     leaveTo="opacity-0 scale-95"
                 >
-                    <Dialog.Panel className="w-3/4 h-3/4 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                    <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900 text-center"
-                    >
-                        ADD FILE
-                    </Dialog.Title>
-                    <div className="mt-2 w-full h-full">
-                            {currentFile && <iframe className='w-full h-3/4 overflow-auto border-8 border-blue-400 rounded-md' src={currentFile} title={"current file"}/>}
-                        <div className="w-full pt-4">
-                        <input 
-                            className='w-full py-2 file:bg-queens-blue file:text-white file:rounded-md bg-page-bg text-black rounded-md p-1'
-                            type='file'
-                            onChange={(e) => handleUploadFile(e.target.files)}
-                        />
-                        </div>
+                    <Dialog.Panel className="w-full h-4/5 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <div className="w-full h-full grid grid-cols-2 gap-x-4">
+                        <div className="w-full h-full">
                             <Dialog.Title
                                 as="h3"
-                                className="text-lg font-medium leading-6 text-gray-900 text-center pt-4"
+                                className="text-lg font-medium leading-6 text-gray-900 text-center"
+                            >
+                                ADD FILE
+                            </Dialog.Title>
+                            <div className="mt-2 w-full h-full">
+                                    {currentFile && <iframe className='w-full h-3/4 overflow-auto border-8 border-blue-400 rounded-md' src={currentFile} title={"current file"}/>}
+                                <div className={`w-full ${currentFile && 'pt-2'}`}>
+                                <input 
+                                    className='w-full file:h-full bg-page-bg text-black rounded-md file:rounded-l-md file:bg-queens-blue file:px-4 file:py-2 file:text-sm file:font-medium file:text-white file:hover:bg-blue-400 file:focus:outline-none file:focus-visible:ring-2 file:focus-visible:ring-white file:focus-visible:ring-opacity-75'
+                                    type='file'
+                                    onChange={(e) => handleUploadFile(e.target.files)}
+                                />
+                            </div>
+                        </div>
+                        </div>
+                        <div className='w-full h-full'>
+                            <Dialog.Title
+                                as="h3"
+                                className="text-lg font-medium leading-6 text-gray-900 text-center"
                             >
                                 ADD RECIPIENT(S) 
                             </Dialog.Title>
-                        <div className="mt-2 w-full flex">
-                            <input
-                                className='w-full py-2 bg-page-bg text-black rounded-l-md p-1'
-                                value={currRecipient}
-                                onChange={(e) => setCurrRecipient(e.target.value)}
-                            />
-                            <button 
-                            className="rounded-r-md bg-queens-blue px-4 py-2 text-sm font-medium text-white hover:bg-blue-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                            onClick={() => handleAddRecipient()}>Add</button>
-                        </div>
-                        <div className='text-center py-2 text-white'>
-                            <div className="w-full rounded-md flex flex-col items-center">
-                                {msg && <p className={`${msg.color} text-center py-1 text-white rounded-md w-1/3`}>{msg.message}</p>}
+                            <div className="mt-2 w-full flex pb-2">
+                                <input
+                                    className='w-full py-2 bg-page-bg text-black rounded-l-md p-1'
+                                    value={currRecipient}
+                                    onChange={(e) => setCurrRecipient(e.target.value)}
+                                />
+                                <button 
+                                className="rounded-r-md bg-queens-blue px-4 py-2 text-sm font-medium text-white hover:bg-blue-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                                onClick={() => handleAddRecipient()}>Add</button>
                             </div>
-                            {listOfRecipients.map((recipient) => {return <p className='text-black'>{recipient}</p>})}
+                            <div className='text-center py-2 text-white'>
+                                <div className="w-full rounded-md flex flex-col items-center">
+                                    {msg && <p className={`${msg.color} text-center py-1 text-white rounded-md w-1/3`}>{msg.message}</p>}
+                                </div>
+                                {listOfRecipients.map((recipient) => {return <p className='text-black'>{recipient}</p>})}
+                            </div>
                         </div>
+
                     </div>
                     </Dialog.Panel>
                 </Transition.Child>
